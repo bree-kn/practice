46a2025920a8644e9f4f31a9bca04ed7cfb04ebb
bree-kn <1335914122@qq.com>
2019-08-19 18:20:56 +0800

    basicknowledge-1 构造方法，成员方法，this,

---
+++ src/main/java/com/example/practice/basicknowledge/Constructor.java
@@ -0,0 +1,95 @@
+package com.example.practice.basicknowledge;
+
+/**
+ * 1.构造方法用来初始化类的一个新对象，并且为实例变量赋予合适的初始值，所以构造方法不需要返回类型
+ * Constructor c = new Constructor();
+ * 但是隐式返回类的对象，即默认返回对象本身。
+ * 2.每个类都会有一个toString方法，当需要用时，可以重写它，则会返回一个字符串如：c.toString()
+ * 如果没有重写这个toString方法时，那么c.toString会返回一个com.example.practice.basicknowledge.Constructor@72ea2f77
+ * 所以需要重写
+ * 3.构造方法不能被继承，不能重写，因为构造方法的作用是创建对象并对变量初始化，如果重写了之后，
+ * 创建父类的对象就会有影响。
+ * 4.子类构造方法中第一行是super(),因为子类继承了父类的成员变量，在使用前必须给其初始化，所以就调用super
+ * 5.构造方法不能私有的，否则其他类不能调用，protected,public默认,都可以调用
+ * 6.构造方法可以是默认修饰符，即什么都不写Constructor(String name, int age) {}
+ */
+public  class Constructor {
+    private String name;
+    private int age;
+    public static String grop;
+
+     Constructor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    //    abstract void dd();//public abstract class Constructor抽象方法的类必须是抽象类，用abstract修饰
+
+    //成员方法中（静态非静态）都不能定义static的变量，因为方法中定义的只能是局部变量，只能在这个方法中访问，
+    //而static的变量是全局变量
+    public  static final void A(){
+//        static int a;
+
+//        System.out.println("grop=="+name);
+        System.out.println(grop);
+        System.out.println(B());
+//        System.out.println(c());//静态方法只能访问静态变量，静态方法
+    }
+    public int c(){
+        return 9;
+    }
+
+    public static int B(){
+        return 9;
+    }
+
+    @Override
+    public String toString() {
+        return "Constructor{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        Constructor c = new Constructor();//抽象类不能实例化
+        c.setAge(20);
+        c.setName("aa");
+//        System.out.println(c.getAge());
+//        System.out.println(c.toString());
+        c.A();
+    }
+    public Constructor(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+class C{
+    private static int a;
+    private int b;
+
+    public static int getA() {
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Constructor c1 = new Constructor("mm",3);
+        System.out.println(c1.getAge());
+        System.out.println(getA());
+    }
+}
---
+++ src/main/java/com/example/practice/basicknowledge/This.java
@@ -0,0 +1,51 @@
+package com.example.practice.basicknowledge;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * 1.this()只能在构造方法的第一行，不能在普通方法中
+ * 2.new一个对象
+ * A a = new A();此时a被占用着，即new A返回一个地址，这个地址赋值给引用a了，java认为这个对象是有用的，不会回收对象，
+ * 如果之后执行了
+ * a = null 或 a = new A();则这个a不在持有之前对象的地址了，变成了空或者新的地址，即a指向空或指向新的地址，则之前的对象就
+ * 无法访问方法了，那么gc垃圾回收器就会认为这个对象没用了，将他从内存中释放掉。
+ * 即没用人引用这个对象的话，就会被回收掉。
+ */
+public class This {
+    private String name;
+    private int age;
+
+    public This(String name){
+        this.name = name;
+    }
+
+    public This(String name, int age) {
+        this.name = name;
+        this.age = age;
+//        this("i am");//如果把this放在这个位置，那么这个this传过来的值会覆盖上边的值的内容，即name会覆盖上边name,所以只能放第一行不覆盖其他的
+    }
+
+    public int A(){
+        System.out.println(this.name);
+        return this.age;
+    }
+
+    public void B(){
+        System.out.println(this.A());
+    }
+    public static void main(String[] args) {
+        //String a 只是声明了一个变量，存储在栈中，String b = new String();不仅在栈中声明了这个变量，同时还在堆中划分了一片区域，变量存储在这片区域。
+        //String a = "abc";表示堆中填充了内容，不一定创建对象了，先在栈中查找有没有这个对象，如果有，则用已存在的abc，没有则创建一个对象
+        String a;//a是一个引用，没有指向一个对象。
+        String b = new String();
+        String c = "i am c";
+//        System.out.println(a);
+        System.out.println(b == null);//false,因为b指向了一个对象，所以不等于null
+        System.out.println(c);
+/*
+        This t = new This("a",3);
+        System.out.println(t.name);
+//        System.out.println(this.name);
+*/
+    }
+}
